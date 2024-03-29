ca657d4ebbae873266f0860b9b29dc47ef56c7b7
andyfullstack <andriikoliadenko@gmail.com>
2024-03-29 15:22:18 +0200

    upd

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '200vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#723b3b',
+        backgroundColor: '#111',
+      }}
+    >
+      React homework template
+    </div>
+  );
+};
